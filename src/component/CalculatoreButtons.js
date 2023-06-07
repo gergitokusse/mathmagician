0ca0cce6bd0f86edc435e1979ed2890099973cf5
Gergito Kusse <gergito2@gmail.com>
2023-06-07 22:38:12 +0300

    update component

--- src/component/CalculatoreButtons.js
+++ src/component/CalculatoreButtons.js
@@ -101,24 +101,4 @@ export function Qutes() {
   );
 }
 
-export function API() {
-  const [data, setData] = useState([]);
-  useEffect(() => {
-    const fetchData = async () => {
-      const res = await fetch('https://jsonplaceholder.typicode.com/todos');
-      const json = await res.json();
-      setData(json);
-    };
-    fetchData();
-  }, [setData]);
-
-  return (
-    <ul>
-      {data.map((item) => (
-        <li key={item.id}>{item.title}</li>
-      ))}
-    </ul>
-  );
-}
-
 export default MyButtons;
