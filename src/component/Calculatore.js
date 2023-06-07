e9ec677bfd7ed3f2c84f258f9a00ebed5fb99a7b
Gergito Kusse <gergito2@gmail.com>
2023-06-07 15:29:45 +0300

    Add event to the calculatore

--- src/component/Calculatore.js
+++ src/component/Calculatore.js
@@ -1,11 +1,31 @@
-import CalButtons from './CalculatoreButtons';
+import React from 'react';
+import CalButtons, { MyCalButtons, MyButtons } from './CalculatoreButtons';
+// import calculate from '../logic/calculate';
 
-function Calculatore() {
+export function Calculatore() {
   return (
     <div className="container">
       <input type="text" className="type-area" />
       <CalButtons />
+      <MyCalButtons />
     </div>
   );
 }
-export default Calculatore;
+
+class Mycalculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <MyButtons />
+      </div>
+    );
+  }
+}
+
+export default Mycalculator;
--- src/component/CalculatoreButtons.js
+++ src/component/CalculatoreButtons.js
@@ -1,26 +1,118 @@
+import React, { useState, useEffect } from 'react';
+import calculate from '../logic/calculate';
+
 function CalButtons() {
   return (
     <div className="calbutton">
-      <span>AC</span>
-      <span>+/-</span>
-      <span>%</span>
-      <span className="opration">+</span>
-      <span>7</span>
-      <span>8</span>
-      <span>9</span>
-      <span className="opration">*</span>
-      <span>4</span>
-      <span>5</span>
-      <span>6</span>
-      <span className="opration">-</span>
-      <span>1</span>
-      <span>2</span>
-      <span>3</span>
-      <span className="opration">+</span>
-      <span className="zero">0</span>
-      <span>.</span>
-      <span className="opration">=</span>
+      <span id="AC">AC</span>
+      <span id="+/-">+/-</span>
+      <span id="%">%</span>
+      <span className="opration" id="+">+</span>
+      <span id="7">7</span>
+      <span id="8">8</span>
+      <span id="9">9</span>
+      <span className="opration" id="*">*</span>
+      <span id="4">4</span>
+      <span id="5">5</span>
+      <span id="6">6</span>
+      <span id="-" className="opration">-</span>
+      <span id="1">1</span>
+      <span id="2">2</span>
+      <span id="3">3</span>
+      <span id="+" className="opration">+</span>
+      <span id="0" className="zero">0</span>
+      <span id=".">.</span>
+      <span id="=" className="opration">=</span>
     </div>
   );
 }
+
+export class MyCalButtons extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <div className="calbutton">
+        <button className="btn" type="button">AC</button>
+        <button className="btn" type="button">+/-</button>
+        <button className="btn" type="button">%</button>
+        <button className="btn opration" type="button">+</button>
+        <button className="btn" type="button">7</button>
+        <button className="btn" type="button">8</button>
+        <button className="btn" type="button">9</button>
+        <button className="btn opration" type="button">*</button>
+        <button className="btn" type="button">4</button>
+        <button className="btn" type="button">5</button>
+        <button className="btn" type="button">6</button>
+        <button className="btn opration" type="button">-</button>
+        <button className="btn" type="button">1</button>
+        <button className="btn" type="button">2</button>
+        <button className="btn" type="button">3</button>
+        <button className="btn opration" type="button">+</button>
+        <button className="btn zero" type="button">0</button>
+        <button className="btn" type="button">.</button>
+        <button className="btn opration" type="button">=</button>
+      </div>
+    );
+  }
+}
+
+export const MyButtons = () => {
+  const [total, setTotal] = useState(null);
+  const [next, setNext] = useState(null);
+  const [operation, setOperation] = useState(null);
+
+  useEffect(() => {
+    const MyButtons = document.querySelectorAll('.btn');
+    const btnEventHandler = (e) => {
+      const response = calculate({ total, next, operation }, e.target.innerText);
+      setTotal(response.total);
+      setNext(response.next);
+      setOperation(response.operation);
+    };
+
+    MyButtons.forEach((button) => {
+      button.addEventListener('click', btnEventHandler);
+    });
+
+    return () => {
+      MyButtons.forEach((button) => {
+        button.removeEventListener('click', btnEventHandler);
+      });
+    };
+  }, [total, next, operation]);
+  return (
+    <div className="calbutton">
+      <div className="type-area">
+        {total || 0}
+        { operation }
+        { next }
+      </div>
+      <button className="btn" type="button">AC</button>
+      <button className="btn" type="button">+/-</button>
+      <button className="btn" type="button">%</button>
+      <button className="btn opration" type="button">÷</button>
+      <button className="btn" type="button">7</button>
+      <button className="btn" type="button">8</button>
+      <button className="btn" type="button">9</button>
+      <button className="btn opration" type="button">x</button>
+      <button className="btn" type="button">4</button>
+      <button className="btn" type="button">5</button>
+      <button className="btn" type="button">6</button>
+      <button className="btn opration" type="button">-</button>
+      <button className="btn" type="button">1</button>
+      <button className="btn" type="button">2</button>
+      <button className="btn" type="button">3</button>
+      <button className="btn opration" type="button">+</button>
+      <button className="btn zero" type="button">0</button>
+      <button className="btn" type="button">.</button>
+      <button className="btn opration" type="button">=</button>
+    </div>
+  );
+};
+
 export default CalButtons;
